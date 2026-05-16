25ff6adf93c767f6ef0525814e0668f204040b98
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-05-15 19:49:29 -0500

    add dep firebase-database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
